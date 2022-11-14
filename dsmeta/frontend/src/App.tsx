e527e5291938298adc8fd2e29fd9cfed67bc3477
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-14 20:11:08 -0300

    Other coomponents

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,10 +1,17 @@
-import NotificationButton from "./components/notificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-      <h1>Hello, world!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
