85730d42615cbcbe57ac6b2c6990eaece2f1c1b1
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-16 16:10:53 -0300

    Implemented layers; API tested

---
+++ dsmeta/backend/src/main/java/com/project/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.project.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.dsmeta.entities.Sale;
+import com.project.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	public List<Sale> findSales() {
+		return service.findSales();
+	}
+}
---
+++ dsmeta/backend/src/main/java/com/project/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.project.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
