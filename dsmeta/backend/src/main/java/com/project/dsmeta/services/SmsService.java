78ffc5ae2d05c67dfab080eec35d52be9353b363
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-17 00:51:00 -0300

    Twilio SMS implemented

---
+++ dsmeta/backend/src/main/java/com/project/dsmeta/services/SmsService.java
@@ -0,0 +1,52 @@
+package com.project.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.project.dsmeta.entities.Sale;
+import com.project.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getDayOfMonth()
+				+ "/" + sale.getDate().getMonthValue() 
+				+ "/" + sale.getDate().getYear();
+		
+		String msg = "\nSeller: " + sale.getSeller()
+				+ "\nDate: " + date
+				+ "\nAmount: $" + String.format("%.2f", sale.getAmount());
+		
+		Twilio.init(twilioSid, twilioKey);
+		
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+		
+		Message message = Message.creator(to, from, msg).create();
+		
+		System.out.println(message.getSid());
+	}
+}
