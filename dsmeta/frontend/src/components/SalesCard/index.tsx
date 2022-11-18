00c2b69bbd9202d93f5bb2ecc325122002a58ce0
KayneFerreira <kayne.ferreira@fatec.sp.gov.br>
2022-11-18 16:27:17 -0300

    Implemented Axios, useEffect first request

--- dsmeta/frontend/src/components/SalesCard/index.tsx
+++ dsmeta/frontend/src/components/SalesCard/index.tsx
@@ -3,7 +3,8 @@ import './styles.css';
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function SalesCard() {
 
@@ -12,6 +13,12 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/sales").then(response => {
+      console.log(response.data);
+    })
+  }, []);
 
   return (
     <div className="dsmeta-card">
